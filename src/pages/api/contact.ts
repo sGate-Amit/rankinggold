8c8ddcae16badbc14b76f824d1657e5f84f4942f
arti-webtern <arti.sharma@sgate.in>
2025-05-29 16:24:03 +0530

    Update contact.ts

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,21 +1,15 @@
 import type { APIRoute } from 'astro';
 
 export const post: APIRoute = async ({ request }) => {
-  const formData = await request.formData();
-
-  const name = formData.get('name');
-  const email = formData.get('email');
-  const subject = formData.get('subject');
-  const message = formData.get('message');
-
-  // You can't use console.log in live CF Workers, but it's fine locally
-  console.log({ name, email, subject, message });
+  const body = await request.json();
 
   return new Response(JSON.stringify({
     success: true,
-    message: "Data received!",
+    received: body,
   }), {
     status: 200,
-    headers: { 'Content-Type': 'application/json' }
+    headers: {
+      'Content-Type': 'application/json'
+    },
   });
 };
